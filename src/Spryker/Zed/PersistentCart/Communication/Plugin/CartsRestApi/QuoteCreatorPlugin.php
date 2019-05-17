d18a5908c8f68867bfad58f4a6c9759216d25ca5
voitov <tigran.voitov@spryker.com>
2019-05-17 16:48:43 +0300

    glue-2279 AA review fixes

--- src/Spryker/Zed/PersistentCart/Communication/Plugin/CartsRestApi/QuoteCreatorPlugin.php
+++ src/Spryker/Zed/PersistentCart/Communication/Plugin/CartsRestApi/QuoteCreatorPlugin.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\PersistentCart\Communication\Plugin\CartsRestApi\QuoteCreator;
+namespace Spryker\Zed\PersistentCart\Communication\Plugin\CartsRestApi;
 
 use Generated\Shared\Transfer\QuoteResponseTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
